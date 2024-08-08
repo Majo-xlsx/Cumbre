b9b1a6f764f4c2f3e28c3afad89cf38ff8ae2a3b
Majo-xlsx <mainfantea@unal.edu.co>
2024-08-08 04:15:26 +0000

    modified:   src/main/scala/web/Bootstrap.scala

--- src/main/scala/web/Bootstrap.scala
+++ src/main/scala/web/Bootstrap.scala
@@ -1,8 +1,5 @@
 package web
 
-import org.scalatra._
-import javax.servlet.ServletContext
-
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
     context.mount(new PatientController, "/*")
