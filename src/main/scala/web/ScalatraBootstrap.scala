aca1923c8e481888200aed482277fbb6b7309db7
Majo-xlsx <mainfantea@unal.edu.co>
2024-08-08 08:48:46 -0500

    Update web

---
+++ src/main/scala/web/ScalatraBootstrap.scala
@@ -0,0 +1,16 @@
+package web
+
+import org.scalatra.LifeCycle
+import org.scalatra.ScalatraServlet
+import javax.servlet.ServletContext
+
+object ScalatraBootstrap {
+  def main(args: Array[String]) {
+  }
+}
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context mount new PatientController
+  }
+}
