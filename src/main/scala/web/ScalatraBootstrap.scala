93a4a83a3962e5e86a613c5b5fd47ab92b9bd027
Majo-xlsx <mainfantea@unal.edu.co>
2024-08-08 10:29:46 -0500

    Update ScalatraBootstrap.scala

--- src/main/scala/web/ScalatraBootstrap.scala
+++ src/main/scala/web/ScalatraBootstrap.scala
@@ -5,12 +5,13 @@ import org.scalatra.ScalatraServlet
 import javax.servlet.ServletContext
 
 object ScalatraBootstrap {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
+    println("Iniciando aplicación...")
   }
 }
 
 class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context mount classOf[PatientController]->"/*"
+  override def init(context: ServletContext): Unit = {
+    context.mount(new PatientController, "/*")
   }
 }
