d965bd55aa1cd820fc70fd4b896b9f4433f102ae
Majo-xlsx <mainfantea@unal.edu.co>
2024-08-08 16:34:46 +0000

    modified:   src/main/scala/web/ScalatraBootstrap.scala

--- src/main/scala/web/ScalatraBootstrap.scala
+++ src/main/scala/web/ScalatraBootstrap.scala
@@ -3,10 +3,23 @@ package web
 import org.scalatra.LifeCycle
 import org.scalatra.ScalatraServlet
 import javax.servlet.ServletContext
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder}
 
 object ScalatraBootstrap {
   def main(args: Array[String]): Unit = {
     println("Iniciando aplicación...")
+    val server = new Server(8080) // Configura el puerto 8080
+    val context = new ServletContextHandler(ServletContextHandler.SESSIONS)
+    context.setContextPath("/")
+    
+    val servletHolder = new ServletHolder(classOf[ScalatraServlet])
+    context.addServlet(servletHolder, "/*")
+    
+    server.setHandler(context)
+    
+    server.start()
+    server.join()
   }
 }
 
